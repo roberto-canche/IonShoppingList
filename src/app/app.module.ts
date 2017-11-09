b461a2882adcbc02f08470ba849fc56af1dd47ed
roberto-canche <roberto.92_c@hotmail.com>
2017-11-09 11:47:22 -0500

    añadiendo angularfire2 al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database'
 
 import { MyApp } from './app.component';
 //import { HomePage } from '../pages/home/home';
+import { FIREBASE_CONFIG } from './firebase.credentials';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { MyApp } from './app.component';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyC7mm0OG8wr58CRzy7Y0Be-Wh721EqvpwQ",
+    authDomain: "ionic-572ea.firebaseapp.com",
+    databaseURL: "https://ionic-572ea.firebaseio.com",
+    projectId: "ionic-572ea",
+    storageBucket: "ionic-572ea.appspot.com",
+    messagingSenderId: "455197067717"
+}
